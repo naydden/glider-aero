76a2accda36371dcd69afb2a71fc969357888931
JosepMSerraMoncunill <josepserra1995@gmail.com>
2017-11-16 12:51:41 +0100

    Added function to compute the coordinates of the points of the problem

---
+++ geometry.m
@@ -0,0 +1,9 @@
+function [c,x,y] = geometry (cr,ct,b,Nx,Ny)
+for j=1:Ny
+    c(j)=cr-(cr-ct)/Ny*(i-1);
+    for i=1:Nx
+        x(i,j)=cr/4-c(j)/4+c(j)/Nx*(i-1);
+        y(i,j)=b/(2*Ny)*(j-1);
+    end
+end
+end
