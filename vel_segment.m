aabeddc558b935cbb5055b9a1bdcfede4cc43fec
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-11-17 11:31:35 +0100

    Changed vel induc

--- vel_segment.m
+++ vel_segment.m
@@ -6,9 +6,11 @@ switch cas
         r1 = [x-x_a; y-y_a; z-z_a]
         r2 = [x-x_b; y-y_b; z-z_b]
         V = 1/(4*pi)*(norm(r1)+norm(r2))/(norm(r1)*norm(r2)*(norm(r1)*norm(r2) + r1'*r2))*cross(r1,r2)
-    case 'semi-line-d'
-        
-    case 'semi-line-e'
+    case 'semi-line'
+        ur = [-1 0 0]
         r2 = [x-x_b; y-y_b; z-z_b]
-        V = 1/(4*pi*d)
+        ur2 = r2/norm(r2);
+        V = 1/(4*pi)*cross(ur,r2)/(norm(ur,r2)^2)*(1-ur'*ur2);   
+    case 'semi-line-e'
+
 end
