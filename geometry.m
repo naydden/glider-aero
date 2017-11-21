654b7b1df2c45d982377ba12fd6711c1dafd9d35
JosepMSerraMoncunill <josepserra1995@gmail.com>
2017-11-21 15:16:59 +0100

    Geometry expanden a corrected to actually compute the geometry of the thing, except the z coord, that is not yet implemented

--- geometry.m
+++ geometry.m
@@ -1,9 +1,24 @@
-function [c,x,y] = geometry (cr,ct,b,Nx,Ny)
-for j=1:Ny
-    c(j)=cr-(cr-ct)/Ny*(j-1);
-    for i=1:Nx
-        x(i,j)=cr/4-c(j)/4+c(j)/Nx*(i-1);
-        y(i,j)=b/(2*Ny)*(j-1);
+function [x,y,xp,yp,xc,yc] = geometry (cr,ct,b,Nx,Ny)
+
+for i=1:Nx+1
+    y(i,:)=linspace(0,b/2,Ny+1);
+    yp(i,:)=linspace(0,b/2,Ny+1);
+end
+
+for j=1:Ny+1
+    c=cr-(cr-ct)*y(1,j)/(b/2);
+    for i=1:Nx+1
+        x(i,j)=cr/4-c/4+c*(i-1)/Nx;
+        xp(i,j)=cr/4-c/4+c*(i-1)/Nx+c/(4*Nx);
     end
 end
+
+for j=1:Ny
+    yc(:,j)=(y(i,j)+y(i,j+1))/2;
+    c=cr-(cr-ct)*yc(1,j)/(b/2);
+    for i=1:Nx
+        xc(i,j)=cr/4-c/4+c*(i-1)/Nx+c*3/(4*Nx);
+    end
+end
+
 end
