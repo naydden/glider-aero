00ad33fcd4b0549b10b0b9e68f7334fb296df386
JosepMSerraMoncunill <josepserra1995@gmail.com>
2017-11-28 18:02:29 +0100

    Corrected the computation of aerodynamic moment

--- moment.m
+++ moment.m
@@ -1,14 +1,11 @@
 % Function that returns the momement about the origin of coordinates
 % of the wing given the lift of each element and its position.
-function M = moment(dL,Nx,Ny,Xp)
-    M = 0;
-    for j=1:Ny
-        for i=1:Nx
-                 
-          Xpm = (Xp(i,j)+Xp(i,j+1))/2;
-          M = M + Xpm*dL(i,j);
-          
-        end
-    end
-    M = -M;
+function M = moment(dL,Vortex,ground)
+x=0.5*(Vortex(1,:,1)+Vortex(2,:,1))';
+dM=-dL.*x;
+if ground==0
+    M=sum(dM);
+else
+    M=sum(dM(1:0.5*size(dM)));
+end
 end
