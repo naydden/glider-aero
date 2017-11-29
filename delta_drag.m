5b21e62b3f8be9bf065443f741497a763a47cf26
SilviaGonzalez <silviaggb7@gmail.com>
2017-11-29 10:09:30 +0100

    Added the different airplane configurations to the function

--- delta_drag.m
+++ delta_drag.m
@@ -3,41 +3,38 @@
 
 function dDind = delta_drag(vortice_mat,control,Gamma,b,Nx,Ny,rho,Uinf)
 
-    deltaY = b/(2*Ny);
-    dDind = zeros(Nx,Ny);
-    N=Nx*Ny;
-   
     w = w_coef(Nx,Ny,vortice_mat,control,Uinf,Gamma);
-     
-    wleft=w(1:N/2);
-    wright=w(N/2+1:N);
-    wMatLeft = zeros(Nx,Ny/2);
-    wMatRight = zeros(Nx,Ny/2);
+  
+    n = length(Gamma);
+    w_new = zeros(Nx,2*Ny);
+    Gamma_new = zeros(Nx,2*Ny);
+    dDind = zeros(Nx,Ny*n);  
     
-    gammaleft=Gamma(1:N/2);
-    gammaright=Gamma(N/2+1:N);
-    gammaMatLeft = zeros(Nx,Ny/2);
-    gammaMatRight = zeros(Nx,Ny/2);
-    
-   
-    for i = 1:Nx
-        for j = 1:Ny/2
-            wMatLeft(i,j)=wleft((i-1)*Ny/2+j);
-            wMatRight(i,j)=wright((i-1)*Ny/2+j);
-            gammaMatLeft(i,j) = gammaleft((i-1)*Ny/2+j);
-            gammaMatRight(i,j) = gammaright((i-1)*Ny/2+j);
+    for Nw = 1:ceil(n/2);
+        for i = 1:Nx
+            
+            if Nw == 3 | Nw == 5  %Cas de l'estabilitzador vertical
+                for j = 1:Ny
+                    w_new(i,j) = w((i-1)*2*Ny + j + 4*Ny*(Nw-1));
+                    Gamma_new(i,j) = Gamma((i-1)*2*Ny + j + 4*Ny*(Nw-1));
+                end
+            else   % Cas ala i estabilitzador horitzontal
+                for j = 1:2*Ny
+                    w_new(i,j) = w((i-1)*2*Ny + j + 4*Ny*(Nw-1));
+                    Gamma_new(i,j) = Gamma((i-1)*2*Ny + j + 4*Ny*(Nw-1)); 
+                end
+            end
         end
     end
     
-    w_new=[wMatLeft, wMatRight];
-    gamma_new=[gammaMatLeft, gammaMatRight];
-    
-    for j=1:Ny
+    for Nw = Nw = 1:ceil(n/2);      
         for i=1:Nx
-            if i == 1
-                dDind(i,j) = rho*gamma_new(1,j)*w_new(1,j)*deltaY;
-            else
-                dDind(i,j) = rho*(gamma_new(i,j)- gamma_new(i-1,j))*w_new(i,j)*deltaY;
+            for j=1:Ny*n
+                if i == 1
+                    dDind(i,j) = rho*Gamma_new(1,j)*w_new(1,j)*deltaY(Nw);
+                else
+                    dDind(i,j) = rho*(Gamma_new(i,j)- Gamma_new(i-1,j))*w_new(i,j)*deltaY(Nw);
+                end
             end
         end
     end
