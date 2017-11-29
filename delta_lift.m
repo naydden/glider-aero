c680e7554019c1a07220ce3c1c666d465725627f
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-11-29 14:08:23 +0100

    Modifed lift function so that to compute for several cases

--- delta_lift.m
+++ delta_lift.m
@@ -1,67 +1,51 @@
-% function dL = delta_lift(Vortex,Gamma,rho,Uinf)
-%     deltaY=(Vortex(2,:,2)-Vortex(1,:,2))';
-%     deltaZ=(Vortex(2,:,3)-Vortex(1,:,3))';
-%     delta=sqrt(deltaY.^2+deltaZ.^2);
-%     dL = rho*norm(Uinf)*Gamma.*delta;
-% end
-
-function dL = delta_lift(Gamma,b,Nx,Ny,rho,Uinf,cas)
+function L = lift(Gamma,b,Nx,Ny,rho,Uinf,cas)
     deltaY = b/(2*Ny);
-    dL = zeros(Nx,Ny);
-    Gamma_e = zeros(Nx,Ny);
-    Gamma_d = zeros(Nx,Ny);  
+    dLw = zeros(Nx,Ny);
+    dLh = zeros(Nx,Ny);
+    dLv = zeros(Nx,Ny);
     switch cas
         case 'ala'
-            k = 0;
-            % semiala esquerra            
-            for i=1:Nx
-                for j=1:Ny
-                    % change from vector to matrix
-                    k = k + 1;
-                    Gamma_e(i,j) = Gamma((i-1)*Ny+j);
-                end
-            end
-            % semiala esquerra
-            Ny_aux = k+Ny;
-            l = 0;
-            for i=1:Nx
-                for j=k:Ny_aux
-                    l = l + 1;
-                    % change from vector to matrix
-                    Gamma_d(i,l) = Gamma((i-1)*Ny+j);
-                end
-            end
-            Gamma = [Gamma_e Gamma_d];
+            Gamma_w = rearrange_wing(Nx,Ny,Gamma,'wing');
             for i=1:Nx
                 for j=1:Ny
                     if i == 1
-                        dL(i,j) = rho*norm(Uinf)*Gamma(1,j)*deltaY;
+                        dLw(i,j) = rho*norm(Uinf)*Gamma_w(1,j)*deltaY;
                     else
-                        dL(i,j) = rho*norm(Uinf)*(Gamma(i,j)-Gamma(i-1,j))*deltaY;
+                        dLw(i,j) = rho*norm(Uinf)*(Gamma_w(i,j)-Gamma_w(i-1,j))*deltaY;
                     end
                 end
             end
-%         case 'ala'
-%             Gamma_new = zeros(Nx,2*Ny);
-%             for i=1:Nx
-%                 for j=1:(Ny*2)
-%                     if(j<=Ny)
-%                         Gamma_new(i,j) = Gamma((i-1)*Ny+j);
-%                     else
-%                         Gamma_new(i,j) = Gamma((i-1)*Ny+j);
-%                     end
-%                         
-%                     Gamma_d(i,l) = Gamma((i-1)*Ny+j);
-%                     if i == 1
-%                         dL(i,j) = rho*norm(Uinf)*Gamma_new(1,j)*deltaY;
-%                     else
-%                         dL(i,j) = rho*norm(Uinf)*(Gamma_new(i,j)-Gamma_new(i-1,j))*deltaY;
-%                     end
-%                 end
-%             end
         case 'ala+htp'
+            Gamma_w = rearrange_wing(Nx,Ny,Gamma,'wing');
+            Gamma_h = rearrange_wing(Nx,Ny,Gamma,'htp');
+            for i=1:Nx
+                for j=1:Ny
+                    if i == 1
+                        dLw(i,j) = rho*norm(Uinf)*Gamma_w(1,j)*deltaY;
+                        dLh(i,j) = rho*norm(Uinf)*Gamma_h(1,j)*deltaY;
+                    else
+                        dLw(i,j) = rho*norm(Uinf)*(Gamma_w(i,j)-Gamma_w(i-1,j))*deltaY;
+                        dLh(i,j) = rho*norm(Uinf)*(Gamma_h(i,j)-Gamma_h(i-1,j))*deltaY;
+                    end
+                end
+            end            
         case 'ala+htp+vtp'
-        case 'ala+htps'
+            Gamma_w = rearrange_wing(Nx,Ny,Gamma,'wing');
+            Gamma_h = rearrange_wing(Nx,Ny,Gamma,'htp');
+            Gamma_v = rearrange_wing(Nx,Ny,Gamma,'vtp');
+            for i=1:Nx
+                for j=1:Ny
+                    if i == 1
+                        dLw(i,j) = rho*norm(Uinf)*Gamma_w(1,j)*deltaY;
+                        dLh(i,j) = rho*norm(Uinf)*Gamma_h(1,j)*deltaY;
+                        dLv(i,j) = rho*norm(Uinf)*Gamma_v(1,j)*deltaY;
+                    else
+                        dLw(i,j) = rho*norm(Uinf)*(Gamma_w(i,j)-Gamma_w(i-1,j))*deltaY;
+                        dLh(i,j) = rho*norm(Uinf)*(Gamma_h(i,j)-Gamma_h(i-1,j))*deltaY;
+                        dLv(i,j) = rho*norm(Uinf)*(Gamma_v(i,j)-Gamma_v(i-1,j))*deltaY;
+                    end
+                end
+            end             
     end
 end
     
---
+++ rearrange_wing.m
@@ -0,0 +1,61 @@
+function Gamma = rearrange_wing(Nx,Ny,Gamma,cas)
+    switch cas
+        case 'wing'
+            Gamma_e = zeros(Nx,Ny);
+            Gamma_d = zeros(Nx,Ny);  
+            k = 0;
+            % semiala esquerra            
+            for i=1:Nx
+                for j=1:Ny
+                    % change from vector to matrix
+                    k = k + 1;
+                    Gamma_e(i,j) = Gamma((i-1)*Ny+j);
+                end
+            end
+            % semiala dreta
+            Ny_aux = k+Ny;
+            for i=1:Nx
+                l = 0;
+                for j=k:Ny_aux
+                    l = l + 1;
+                    % change from vector to matrix
+                    Gamma_d(i,l) = Gamma((i-1)*Ny+j);
+                end
+            end
+            Gamma = [Gamma_e Gamma_d];
+        case 'vtp'
+            Gamma_new = zeros(Nx,Ny);
+            start = Nx*Ny*4+1;
+            for i=1:Nx
+                for j=start:(start+Ny)
+                    % change from vector to matrix
+                    Gamma_new(i,j) = Gamma((i-1)*Ny+j);
+                end
+            end
+            Gamma = Gamma_new;
+        case 'htp'
+            Gamma_e = zeros(Nx,Ny);
+            Gamma_d = zeros(Nx,Ny);
+            start = Nx*2*Ny+1;
+            k = 0;
+            % semiala esquerra            
+            for i=1:Nx
+                for j=start:(start+Ny)
+                    % change from vector to matrix
+                    k = k + 1;
+                    Gamma_e(i,j) = Gamma((i-1)*Ny+j);
+                end
+            end
+            % semiala dreta
+            start = start + k;
+            for i=1:Nx
+                l = 0;
+                for j=start:(start+Ny)
+                    l = l + 1;
+                    % change from vector to matrix
+                    Gamma_d(i,l) = Gamma((i-1)*Ny+j);
+                end
+            end
+            Gamma = [Gamma_e Gamma_d];         
+    end
+end            
