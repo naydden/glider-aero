453e906add3650acc511356ae504bd7695a2c7c2
laurapla <laurazhuh@gmail.com>
2017-11-30 01:43:01 +0100

    Function modified
    He canviat la funció per adaptar-la a la nova forma de calcular el CL. No sé si funciona, però ara tinc son.

--- ZLangle.m
+++ ZLangle.m
@@ -1,6 +1,10 @@
 % Function that gives the zero lift angle of attack of the wing (in the
 % central section)
-function alpha = ZLangle(cr,ct,b,Nx,Ny,m,p,sweep,dihedral,twist,x_offset,z_offset,rho)
+function alpha = ZLangle(cr_W,ct_W,b_W,Nx,Ny,m_W,p_W,sweep_W,dihedral_W,twist,x_offset_W,z_offset_W,rho)
+
+% Geometry
+[~,Vortex,ControlP,DragP,Normal] = wing_assembly (cr_W,ct_W,b_W,...
+    Nx,Ny,m_W,p_W,sweep_W,dihedral_W,twist_W,x_offset_W,z_offset_W);
 
 alpha0 = -twist/4; % initial value of the iteration
 dalpha = 0.01; % alpha increment
@@ -16,8 +20,17 @@ while resta>delta
     CLantant = CLant;
     CLant = CL;
     
-    Uinf= [1*cosd(alpha),0,1*sind(alpha)];
-    [CL, ~] = Coeff(cr,ct,b,Nx,Ny,m,p,sweep,dihedral,twist,x_offset,z_offset,Uinf,rho);
+    Uinf = [1*cosd(alpha),0,1*sind(alpha)];
+    
+    % Computations
+    Gamma = circulation(Uinf,Vortex,ControlP,Normal);
+    [dLw,dLh,dLv] = delta_lift(Gamma,b_W,Nx,Ny,rho,Uinf,'ala');
+    dDind = delta_drag(Vortex,DragP,Gamma,deltaY,Nx,Ny,rho,Uinf);
+    
+    L = lift(dLw,dLh,dLv);
+    M = moment(dLw,dLh,dLv,Nx,Ny,DragP(:,:,1),'ala');
+    Dind = drag(dDind,Nx,Ny);
+    [CL, ~, ~] = Coeff(cr_W,ct_W,b_W,Uinf,rho,L,Dind,M);
     resta = abs(CL);
     
     
