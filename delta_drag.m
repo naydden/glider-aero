d458002aa1e91fd772cf5738ccebebaa6920db98
SilviaGonzalez <silviaggb7@gmail.com>
2017-11-30 17:30:13 +0100

    Errors corrections

--- delta_drag.m
+++ delta_drag.m
@@ -7,7 +7,11 @@ function dDind = delta_drag(vortice_mat,control,Gamma,deltaY,Nx,Ny,rho,Uinf)
   
     n = length(Gamma);
     Nw = ceil(n/(Nx*Ny));
-    if Nw>5 n=n/2; end
+    
+    if Nw>5 | Nw==4
+        n=n/2; Nw=Nw/2;
+    end
+    
     w_new = zeros(Nx,Ny*Nw);
     Gamma_new = zeros(Nx,Ny*Nw);
     dDind = zeros(Nx,Ny*Nw);  
@@ -18,23 +22,25 @@ function dDind = delta_drag(vortice_mat,control,Gamma,deltaY,Nx,Ny,rho,Uinf)
                 for j = 1:Ny
                     w_new(i,j+2*Ny*(Nw-1)) = w((i-1)*Ny + j + 4*Ny*(Nw-1));
                     Gamma_new(i,j+2*Ny*(Nw-1)) = Gamma((i-1)*Ny + j + 4*Ny*(Nw-1));
+                    
+                    if i == 1
+                        dDind(i,j+2*Ny*(Nw-1)) = rho*Gamma_new(i,j+2*Ny*(Nw-1))*w_new(1,j)*deltaY(Nw);
+                    else
+                        dDind(i,j+2*Ny*(Nw-1)) = rho*(Gamma_new(i,j+2*Ny*(Nw-1))- Gamma_new(i-1,j+2*Ny*(Nw-1)))*w_new(i,j+2*Ny*(Nw-1))*deltaY(Nw);
+                    end
+                    
                 end
             else   % Cas ala i estabilitzador horitzontal
                 for j = 1:2*Ny
                     w_new(i,j+2*Ny*(Nw-1)) = w((i-1)*2*Ny + j + 4*Ny*(Nw-1));
                     Gamma_new(i,j+2*Ny*(Nw-1)) = Gamma((i-1)*2*Ny + j + 4*Ny*(Nw-1)); 
-                end
-            end
-        end
-    end
-    
-    for Nw = 1:ceil(n/(2*Nx*Ny))      
-        for i=1:Nx
-            for j=1:Ny*Nw
-                if i == 1
-                    dDind(i,j) = rho*Gamma_new(1,j)*w_new(1,j)*deltaY(Nw);
-                else
-                    dDind(i,j) = rho*(Gamma_new(i,j)- Gamma_new(i-1,j))*w_new(i,j)*deltaY(Nw);
+                   
+                    if i == 1
+                        dDind(i,j+2*Ny*(Nw-1)) = rho*Gamma_new(i,j+2*Ny*(Nw-1))*w_new(1,j)*deltaY(Nw);
+                    else
+                        dDind(i,j+2*Ny*(Nw-1)) = rho*(Gamma_new(i,j+2*Ny*(Nw-1))- Gamma_new(i-1,j+2*Ny*(Nw-1)))*w_new(i,j+2*Ny*(Nw-1))*deltaY(Nw);
+                    end
+                    
                 end
             end
         end
